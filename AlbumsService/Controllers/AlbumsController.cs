3cfb514e8e5418f2980631ed0118f3c0e98c19ea
LaurenLingham <lauren.lingham@outlook.com>
2023-06-11 16:48:18 +0100

    created albums service with CRUD endpoints

---
+++ AlbumsService/Controllers/AlbumsController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AlbumsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+        private Dictionary<int, string> _albums = new Dictionary<int, string>();
+
+        public AlbumsController()
+        {
+            _albums.Add(1, "album 1");
+            _albums.Add(2, "album 2");
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return _albums.Values.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return _albums[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+            int id;
+
+            do
+            {
+                id = new Random().Next(1000);
+            }
+            while (_albums.ContainsKey(id));
+
+            _albums.Add(id, value);
+        }
+
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            _albums[id] = value;
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _albums.Remove(id);
+        }
+    }
+}
